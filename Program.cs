a88529f376b7e91ff23de50e47f5410295d265bc
Krystian <krystiankolad@wp.pl>
2017-11-04 16:13:07 +0100

    Fields and methods in Program

--- Program.cs
+++ Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using PEA.Algorithms;
+using PEA.Algorithms.Interfaces;
+using PEA.DataAccess;
+using PEA.DataAccess.Interfaces;
+using PEA.Model;
+using PEA.Model.Interfaces;
+using PEA.Parser;
+using PEA.Parser.Interfaces;
 
 namespace PEA
 {
     class Program
     {
+        private static IReader _reader;
+        private static IParser<City> _parser;
+        private static IAlgorithm _algorithm;
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        private static void SymmetricalGeneticTSP()
+        {
+            _reader = new SymmetricalReader();
+            _parser = new SymmetricalParser();
+            _algorithm = new GeneticAlgorithm();
+        }
+
+        private static void AsymmetricalGeneticTSP()
+        {
+            _reader = new AsymmetricalReader();
+            _parser = new AsymmetricalParser();
+            _algorithm = new GeneticAlgorithm();
         }
     }
 }
