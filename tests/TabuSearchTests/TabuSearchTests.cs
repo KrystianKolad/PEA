b07ec7a3388126bb8ddac86f645b80a06cc54f1a
Krystian <krystiankolad@wp.pl>
2017-12-19 15:52:17 +0100

    Dodanie zapisywania plików w formie tabelki oraz wykresu

--- tests/TabuSearchTests/TabuSearchTests.cs
+++ tests/TabuSearchTests/TabuSearchTests.cs
@@ -21,7 +21,8 @@ namespace TabuSearchTests
         IParser<Matrix> _TSPParser;
         IParser<Matrix> _ATSPParser;
         AbstractTabuSearchAlgorithm _algorithm;
-        Dictionary<string, double> _results;
+        Dictionary<string, double> _tspResults;
+        Dictionary<string, double> _atspResults;
         public TabuSearchTests()
         {
             _TSPFileReader = new TSPFileReader();
@@ -30,7 +31,8 @@ namespace TabuSearchTests
             _ATSPParser = new AsymmetricalParser();
             _algorithm = new TabuSearchAlgorithm();
             GetFileNames();
-            _results = new Dictionary<string, double>();
+            _tspResults = new Dictionary<string, double>();
+            _atspResults = new Dictionary<string, double>();
         }
         [Fact]
         public void CrateData()
@@ -39,27 +41,105 @@ namespace TabuSearchTests
             {
                 for(int j = 1; j<4;j++)
                 {
-                _results.Clear();
+                _tspResults.Clear();
+                _atspResults.Clear();
                 foreach (var file in TSPFiles)
                 {
                     var time = _algorithm.Execute(_TSPParser.ParseData(_TSPFileReader.Read(file)),(int)Math.Pow(10,i),5*j);
-                    _results.Add(file, time);
+                    _tspResults.Add(file, time);
                 }
                 foreach (var file in ATSPFiles)
                 {
                     var time = _algorithm.Execute(_ATSPParser.ParseData(_ATSPFileReader.Read(file)),(int)Math.Pow(10,i),5*j);
-                    _results.Add(file, time);
+                    _atspResults.Add(file, time);
                 }
-                using (var streamWriter = new StreamWriter($"../../../../../artifacts/tabuSearchTest{i}_{j}.txt"))
+                using (var streamWriter = new StreamWriter($"../../../../../artifacts/tabuSearchTest{Math.Pow(10,i)}_{5*j}.txt"))
                 {
-                    streamWriter.WriteLine(@"Plik       Wartosc[ms]");
-                    foreach (var result in _results)
+                    streamWriter.WriteLine(@"\begin{center}");
+                    streamWriter.WriteLine(@"\begin{table}");
+                    streamWriter.WriteLine($"\\caption{{Symetryczny problem dla {Math.Pow(10,i)} iteracji oraz kadencji = {5*j}}}");
+                    streamWriter.WriteLine(@"\begin{table}");
+                    streamWriter.WriteLine(@"\begin{tabular}{ |c|c| } ");
+                    streamWriter.WriteLine(@" \hline");
+                    streamWriter.WriteLine(@"Plik(wraz z ilością miast) & wynik \\ \hline");
+                    foreach (var result in _tspResults)
                     {
                         var key = result.Key.Split(@"/");
                         var file = key[key.Length-1];
-                        Console.WriteLine(file + ": " + result.Value.ToString());
-                        streamWriter.WriteLine(file +"      " + result.Value.ToString());
+                        streamWriter.WriteLine(file +" & " + result.Value.ToString() + @"\\ \hline");
                     }
+                    streamWriter.WriteLine(@"\end{tabular}");
+                    streamWriter.WriteLine(@"\end{table}");
+                    streamWriter.WriteLine(@"\end{center}");
+                    streamWriter.WriteLine("");
+                    streamWriter.WriteLine(@"\begin{center}");
+                    streamWriter.WriteLine(@"\begin{table}");
+                    streamWriter.WriteLine($"\\caption{{Asymetryczny problem dla {Math.Pow(10,i)} iteracji oraz kadencji = {5*j}}}");
+                    streamWriter.WriteLine(@"\begin{table}");
+                    streamWriter.WriteLine(@"\begin{tabular}{ |c|c| } ");
+                    streamWriter.WriteLine(@" \hline");
+                    streamWriter.WriteLine(@"Plik(wraz z ilością miast) & wynik");
+                    foreach (var result in _atspResults)
+                    {
+                        var key = result.Key.Split(@"/");
+                        var file = key[key.Length-1];
+                        streamWriter.WriteLine(file +" & " + result.Value.ToString() + @"\\ \hline");
+                    }
+                    streamWriter.WriteLine(@"\end{tabular}");
+                    streamWriter.WriteLine(@"\end{table}");
+                    streamWriter.WriteLine(@"\end{center}");
+                    streamWriter.WriteLine("";)
+                    streamWriter.WriteLine($"\\subsubsection{{Wykres zależności czasu wykonywania od ilości miast dla symetrycznego problemu przy ilości iteracji ={Math.Pow(10,i)} oraz kadencji = {5*j} }}");
+                    streamWriter.WriteLine(@"\begin{tikzpicture}[scale=1.1]
+                                            \begin{axis}[
+                                            xlabel={Liczba miast},
+                                            ylabel={Czas[ms]},
+                                            xmin=0,xmax=26,
+                                            ymin=0,ymax=100,
+                                            legend pos=north west,
+                                            ymajorgrids=true,grid style=dotted
+                                            ]
+
+                                            \addplot[color=blue,mark=square]
+                                            coordinates {")
+                    foreach (var result in _tspResults)
+                    {
+                        var key = result.Key.Split(@"/");
+                        var file = key[key.Length-1];
+                        streamWriter.WriteLine("("file +"," + result.Value.ToString() + ")");
+                    }
+                    streamWriter.WriteLine(@"};
+
+                                            \end{axis}
+                                            \end{tikzpicture}
+                                            ")
+                    streamWriter.WriteLine("";)
+                    streamWriter.WriteLine($"\\subsubsection{{Wykres zależności czasu wykonywania od ilości miast dla niesymetrycznego problemu przy ilości iteracji ={Math.Pow(10,i)} oraz kadencji = {5*j} }}");
+                    streamWriter.WriteLine(@"\begin{tikzpicture}[scale=1.1]
+                                            \begin{axis}[
+                                            xlabel={Plik},
+                                            ylabel={Czas[ms]},
+                                            xmin=0,xmax=26,
+                                            ymin=0,ymax=100,
+                                            legend pos=north west,
+                                            ymajorgrids=true,grid style=dotted
+                                            ]
+
+                                            \addplot[color=blue,mark=square]
+                                            coordinates {")
+                    foreach (var result in _atspResults)
+                    {
+                        var key = result.Key.Split(@"/");
+                        var file = key[key.Length-1];
+                        streamWriter.WriteLine("("file +"," + result.Value.ToString() + ")");
+                    }
+                    streamWriter.WriteLine(@"};
+
+                                            \end{axis}
+                                            \end{tikzpicture}
+                                            ")
+
+                    
                 }
                 }
             }
